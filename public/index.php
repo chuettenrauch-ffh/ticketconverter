e0a4640f5f0703c585e6f2d11a8edb07a0a8d830
cjaecks <christoph.jaecks@fashionforhome.de>
2014-04-30 17:43:03 +0200

    Test commit

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-
 	<title>Ticketconverter</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<link rel="shortcut icon" href="icon.ico" type="image/x-icon"/>
